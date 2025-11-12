dca43690f4525b97467948d8c66ad1335c789a4c
WatashiSenseiGalirus <edi23956@gmail.com>
2025-11-12 09:03:10 +0700

    Update database: 12/11/2025 09:03 - 68 users

--- database_ID.sh
+++ database_ID.sh
@@ -72,3 +72,5 @@ Nama: Galirus Whoami: u0_a235 Expired 2hari
 Nama: Rapp Whoami: u0_a443 Expired Permanent
 
 Nama: Rusdy Whoami: u0_a456 Expired Permanent
+
+Nama: wanwan Whoami: u0_a201 Expired 1bulan

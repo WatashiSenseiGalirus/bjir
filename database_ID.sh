544693c73dcf27002e776d5d303e0bec77462314
WatashiSenseiGalirus <58677185+WatashiSenseiGalirus@users.noreply.github.com>
2025-11-23 06:48:08 +0700

    Update database_ID.sh

--- database_ID.sh
+++ database_ID.sh
@@ -75,4 +75,4 @@ Nama: ivan Whoami: u0_a454 16/11/2026|Permanent
 Nama: bagus Whoami: u0_a727 16/11/2026|7hari
 Nama: farel Whoami: u0_a63 21/11/2026|Permanent
 Nama: vivi Whoami: u0_a156 22/11/2026|Permanent
-Nama: kiki Whoami: u0_a339 24/11/2025|1hari
+Nama: kiki Whoami: u0_a339 24/11/2025|Permanent

8b25d22cba7de8589d09fa2f854a3595a056d9d5
GALIRUS404 <galirusofficial@gmail.com>
2025-11-30 23:18:37 +0700

    Update database: 30/11/2025 23:18 - 79 users

--- database_ID.sh
+++ database_ID.sh
@@ -76,5 +76,4 @@ Nama: farel Whoami: u0_a63 21/11/2026|Permanent
 Nama: vivi Whoami: u0_a156 22/11/2026|Permanent
 Nama: kiki Whoami: u0_a344 24/11/2025|Permanent
 Nama: chll Whoami: u0_a316 24/11/2026|Permanent
-Nama: Izan Whoami: u0_a598 26/11/2025|1hari
 Nama: Riiosaka Whoami: u0_a203 24/11/2026|Permanent

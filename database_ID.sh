472521bc4d54ad32e0ddafb8d10ee0a3ed887efc
WatashiSenseiGalirus <58677185+WatashiSenseiGalirus@users.noreply.github.com>
2025-12-16 23:42:51 +0700

    Update database_ID.sh

--- database_ID.sh
+++ database_ID.sh
@@ -93,3 +93,4 @@ Nama: elstalin Whoami: u0_a323 04/12/2026|Permanent
 Nama: BloodsKiL Whoami: u0_a321 14/12/2025|7hari
 Nama: Ofga Whoami: u0_a304 04/12/2026|Permanent
 Nama: mix Whoami: u0_a127 04/12/2026|Permanent
+Nama: Riskii Whoami: u0_a757 04/12/2026|Permanent

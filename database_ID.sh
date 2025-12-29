5f259b18cffe79b5c47fa46c267a1e1f4c570f76
WatashiSenseiGalirus <58677185+WatashiSenseiGalirus@users.noreply.github.com>
2025-12-29 18:34:02 +0700

    Update database_ID.sh

--- database_ID.sh
+++ database_ID.sh
@@ -105,3 +105,4 @@ Nama: Sultan Whoami: u0_a302 23/12/2025|Permanent
 Nama: petani Whoami: u0_a371 27/12/2025|7hari
 Nama: refal Whoami: u0_a218 23/12/2025|Permanent
 Nama: Sukampret Whoami: u0_a439 23/12/2025|Permanent
+Nama: Sultan Whoami: u0_a279 29/12/2025|7hari

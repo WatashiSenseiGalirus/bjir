4b3bffa022f5dbfa4662a1d991b77fce55766aef
WatashiSenseiGalirus <58677185+WatashiSenseiGalirus@users.noreply.github.com>
2026-01-20 17:38:15 +0700

    Update database_ID.sh

--- database_ID.sh
+++ database_ID.sh
@@ -123,3 +123,4 @@ Nama: Yuan Whoami: u0_a422 01/01/2026|Permanent
 Nama: Salza09 Whoami: u0_a767 01/01/2026|Permanent
 Nama: tengil Whoami: u0_a282 01/01/2026|Permanent
 Nama: afan Whoami: u0_a238 01/01/2026|Permanent
+Nama: arif Whoami: u0_a922 01/01/2026|Permanent

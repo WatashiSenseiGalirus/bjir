cfe6c2e9d9fe64a2984b1ab6aef856d5a7ae8896
WatashiSenseiGalirus <58677185+WatashiSenseiGalirus@users.noreply.github.com>
2026-02-07 07:56:17 +0700

    Update database_ID.sh

--- database_ID.sh
+++ database_ID.sh
@@ -1,4 +1,5 @@
 Nama: Dika Whoami: u0_a171 13/11/2025|Permanent
+Nama: Abdi_Soekarno Whoami: u0_a250 13/11/2025|Permanent
 Nama: cybergalaxy Whoami: u0_a530 13/11/2025|Permanent
 Nama: FADHIL Whoami: u0_a345 13/11/2025|Permanent
 Nama: axae Whoami: u0_a271 13/11/2025|Permanent
